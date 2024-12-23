4d9729e2d7755f42625e9c66f551a1169cc53507
bubukaka324 <danpavlov1213@gmail.com>
2024-12-23 12:08:49 +0200

    an

--- beginer.js
+++ beginer.js
@@ -169,6 +169,73 @@ start.addEventListener('click', function showhide(){
     block3.style.display = "none"
 })
 
+//Setup spin animation
+const spin = gsap.timeline({ paused: true });
+spin.from("#snowball", {
+	duration: 5,
+	rotate: 360,
+	transformOrigin: "50%,50%",
+	ease: "expo.out"
+});
+
+//Setup Canvas
+const canvas = document.getElementById("snowball");
+const ctx = canvas.getContext("2d");
+canvas.width = 200;
+canvas.height = 200;
+let snowflakes = [];
+
+function Snowflake(x, y, radius, speed, wind, alpha) {
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	this.speed = speed;
+	this.wind = wind;
+	this.update = function () {
+		this.y += this.speed;
+		this.x += this.wind;
+		if (this.y > canvas.height) {
+			this.y = 0;
+			this.x = Math.random() * canvas.width;
+		}
+	};
+
+	this.draw = function () {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+		ctx.fillStyle = "white";
+		ctx.globalAlpha = alpha;
+		ctx.fill();
+	};
+}
+
+function createSnowflakes() {
+	spin.restart(); //start spin amination
+	snowflakes = []; //Empty Array
+	let count = Math.floor(Math.random() * 301) + 100; //Decide how many snowflakes
+	for (let i = 0; i < count; i++) {
+		let x = Math.random() * canvas.width;
+		let y = Math.random() * canvas.height;
+		let radius = Math.random() * 3;
+		let speed = Math.random() * 0.1 + 1;
+		let wind = Math.random() * 2 - 1;
+		let alpha = Math.random() * 0.6 + 0.4;
+		snowflakes.push(new Snowflake(x, y, radius, speed, wind, alpha));
+	}
+}
+
+function animate() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	snowflakes.forEach((snowflake) => {
+		snowflake.update();
+		snowflake.draw();
+	});
+
+	requestAnimationFrame(animate);
+}
+
+createSnowflakes();
+animate();
 
 
 
